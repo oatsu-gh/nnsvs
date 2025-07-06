0218b978caa6ebdd0771d6311abdebf378fc780b
oatsu <panchi.psp@gmail.com>
2025-07-06 13:27:11 +0900

    applied isort

--- nnsvs/util.py
+++ nnsvs/util.py
@@ -9,10 +9,11 @@ import pkg_resources
 import pyworld
 import torch
 from hydra.utils import instantiate
-from nnsvs.multistream import get_static_features, get_static_stream_sizes
-from nnsvs.usfgan import USFGANWrapper
 from omegaconf import OmegaConf
 from torch import nn
+
+from nnsvs.multistream import get_static_features, get_static_stream_sizes
+from nnsvs.usfgan import USFGANWrapper
 
 try:
     from parallel_wavegan.utils import load_model
