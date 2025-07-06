001eb5f4dbef7b9ea50c3685404741d0ae58ccd1
oatsu <panchi.psp@gmail.com>
2025-07-06 15:17:02 +0900

    Update diffusion.py (repaired isort)

--- nnsvs/diffsinger/diffusion.py
+++ nnsvs/diffsinger/diffusion.py
@@ -3,9 +3,8 @@ from functools import partial
 
 import numpy as np
 import torch
-from tqdm.auto import tqdm
-
 from nnsvs.base import BaseModel, PredictionType
+from tqdm.auto import tqdm
 
 
 def extract(a, t, x_shape):
