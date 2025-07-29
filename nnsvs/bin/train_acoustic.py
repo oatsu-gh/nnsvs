189107809afd2908ad4babf6e93ddf20c5e3879b
oatsu <oatsu.dev@gmail.com>
2025-07-29 23:44:22 +0900

    green tqdm

--- nnsvs/bin/train_acoustic.py
+++ nnsvs/bin/train_acoustic.py
@@ -329,7 +329,9 @@ def train_loop(
         from tqdm.auto import tqdm
 
     train_iter = 1
-    for epoch in tqdm(range(1, config.train.nepochs + 1), desc="Epochs", color="green"):
+    for epoch in tqdm(
+        range(1, config.train.nepochs + 1), desc="Epochs", colour="green"
+    ):
         for phase in data_loaders.keys():
             train = phase.startswith("train")
             # schedulefree optimizers need training
